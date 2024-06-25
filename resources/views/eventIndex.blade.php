198e40007c269f03569de1cebf86705bf337e159
YAHYA <yahyamahmood665@gmail.com>
2024-06-25 13:12:04 +0500

    changes

--- resources/views/eventIndex.blade.php
+++ resources/views/eventIndex.blade.php
@@ -1,5 +1,4 @@
 <x-main-layout>
-    <!-- component -->
     <section class="bg-white dark:bg-gray-900">
         <div class="container px-6 py-10 mx-auto">
             <h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">All Events</h1>
@@ -12,7 +11,7 @@
 
                         <div class="flex flex-col justify-between py-6 lg:mx-6">
                             <a href="{{ route('eventShow', $event->id) }}"
-                                class="text-xl font-semibold text-gray-800 hover:underline dark:text-white ">
+                                class="text-xl font-semibold text-gray-800 hover:underline dark:text-gray ">
                                 {{ $event->title }}
                             </a>
 
@@ -27,6 +26,7 @@
                     </div>
                 @endforeach
             </div>
+
             {{ $events->links() }}
         </div>
     </section>
--- resources/views/eventsShow.blade.php
+++ resources/views/eventsShow.blade.php
@@ -12,7 +12,7 @@
             <div class="flex flex-col">
                 <div class="ripple relative overflow-hidden rounded-lg bg-cover bg-[50%] bg-no-repeat shadow-lg dark:shadow-black/20"
                     data-te-ripple-init data-te-ripple-color="light">
-                    <img src="{{ asset('/storage/' . $event->image) }}" class="w-full" alt="Louvre" />
+                    <img src="{{ asset('/storage/' . $event->image) }}" alt="{{ $event->title }}" class="w-full"  />
                     <a href="#!">
                         <div
                             class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
